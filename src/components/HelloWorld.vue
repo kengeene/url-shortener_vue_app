1b7ca41af4a931d044661d4e99968f3a464189ba
kengeene <eugene@sendyit.com>
2022-09-14 21:02:18 +0300

    Initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,11 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <p class="textBox">Insert your URL here:</p>
+    <input v-model="originalURL" class="textBox" />
+    <button class="textBox" @click="fetchUrl">Convert URL</button>
+    <span class="textBox"
+      ><a :href="shortenedURL" target="blank">{{ shortenedURL }}</a></span
+    >
   </div>
 </template>
 
@@ -34,25 +13,57 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+    age: {
+      type: Number,
+      default: 2,
+    },
+  },
+  data() {
+    return {
+      originalURL: '',
+      shortenedURL: '',
+    }
+  },
+  methods: {
+    async fetchUrl() {
+      const url = new URL('https://t.ly/api/v1/link/shorten')
+
+      const params = {
+        api_token: 'V4BVn8jdbCCdWhvqDepu9kvcf0p7kjJQv3bHhsz4eF3vJkhhNrZ71Z4yumuq',
+      }
+      Object.keys(params).forEach((key) => url.searchParams.append(key, params[key]))
+
+      const headers = {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      }
+
+      let body = {
+        long_url: this.originalURL,
+        domain: 'https://t.ly/',
+        include_qr_code: false,
+      }
+
+      const { short_url } = await fetch(url, {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(body),
+      }).then((response) => response.json())
+
+      this.shortenedURL = short_url
+    },
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+  margin: 0 auto;
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.textBox {
+  display: block;
+  margin: 0;
 }
 </style>
